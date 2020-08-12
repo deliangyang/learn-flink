3ec558fbf79e208059587f6e7ddf49a7fb228526
phantom <623601391@qq.com>
2020-08-12 21:59:18 +0800

    feat: sink kafka

--- src/main/scala/com/kakfa/test/KafkaTest.scala
+++ src/main/scala/com/kakfa/test/KafkaTest.scala
@@ -1,12 +1,13 @@
 package com.kakfa.test
 
+import java.lang
 import java.util.Properties
 
 import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.connectors.kafka._
-import org.apache.flink.streaming.connectors.kafka.internals.KeyedSerializationSchemaWrapper
+import org.apache.kafka.clients.producer.ProducerRecord
 
 
 object KafkaTest {
@@ -24,7 +25,7 @@ object KafkaTest {
     )
     stream.print()
 
-    val counter = stream.flatMap { _.toLowerCase.split("\\W+") filter { _.nonEmpty } }
+    val counter: DataStream[(String, Int)] = stream.flatMap { _.toLowerCase.split("\\W+") filter { _.nonEmpty } }
       .map { (_, 1) }
       .keyBy(0)
       .timeWindow(Time.seconds(5))
@@ -32,16 +33,49 @@ object KafkaTest {
 
     counter.print()
 
-    val producer = new FlinkKafkaProducer(
-      "test",
-      new SimpleStringSchema(),
-      properties
-    )
+    val properties2 = new Properties()
+    properties2.setProperty("bootstrap.servers", "localhost:9092")
 
-    counter.addSink(producer)
+    counter.addSink(new FlinkKafkaProducer[(String, Int)](
+      "test",
+      new Tt("test"),
+      properties2,
+      FlinkKafkaProducer.Semantic.EXACTLY_ONCE
+    ))
+
+//    val producer = new FlinkKafkaProducer(
+//      "test",
+//      new SimpleStringSchema(),
+//      properties
+//    )
+//    val conf = new FlinkJedisPoolConfig.Builder().setHost("10.0.0.11").setPort(7000).build()
+//
+//    val redisSink = new RedisSink[String](conf, new RedisMapper[String] {
+//      override def getCommandDescription: RedisCommandDescription = {
+//        new RedisCommandDescription(RedisCommand.HSET, "sensor")
+//      }
+//
+//      override def getKeyFromData(t: String): String = {
+//        t.split(",")(0)
+//      }
+//
+//      override def getValueFromData(t: String): String = {
+//        t.split(",")(1)
+//      }
+//    })
+//
+//    val a = new RichSinkFunction[String] {}
+//
+//    counter.addSink(a)
 
     env.execute("flink-kafka")
   }
 
+}
 
+
+class Tt(topic: String) extends KafkaSerializationSchema[(String, Int)] {
+  override def serialize(t: (String, Int), aLong: lang.Long): ProducerRecord[Array[Byte], Array[Byte]] = {
+    new ProducerRecord[Array[Byte], Array[Byte]](topic, t._1.getBytes(), t._2.toString.getBytes())
+  }
 }
