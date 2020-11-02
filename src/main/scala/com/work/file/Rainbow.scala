acf6e690f7137fbfb624796aad6f8bf8cdc9409c
phantom <623601391@qq.com>
2020-11-02 21:22:33 +0800

    feat: x

--- src/main/scala/com/work/file/Rainbow.scala
+++ src/main/scala/com/work/file/Rainbow.scala
@@ -1,25 +1,35 @@
 package com.work.file
 
+import java.beans.BeanProperty
+
 import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.api.scala.ExecutionEnvironment
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.{JsonIgnoreProperties, JsonProperty}
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.flink.streaming.api.scala._
 
-import scala.beans.BeanProperty
-
-case class WordCount(word: String, count: Int)
+// case class WordCount(word: String, count: Int)
 
 
 object Rainbow {
 
   @JsonIgnoreProperties(ignoreUnknown = true)
-  case class MyValue(
-                      @JsonProperty("user") @BeanProperty user: Long,
-                      @JsonProperty("ip") @BeanProperty ip: String,
-                      @JsonProperty("path") @BeanProperty path: String,
-                      @JsonProperty("method") @BeanProperty method: String
-                    )
+  case class
+  Extra(
+         @JsonProperty("errno") @BeanProperty errno: Int,
+         @JsonProperty("executionTime") @BeanProperty executionTime: Int,
+         @JsonProperty("upstreamCost") @BeanProperty upstreamCost: Int,
+       )
+
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  case class
+  MyValue(
+           @JsonProperty("ip") @BeanProperty ip: String,
+           @JsonProperty("user") @BeanProperty user: String,
+           @JsonProperty("path") @BeanProperty path: String,
+           @JsonProperty("extra") @BeanProperty extra: Extra,
+           @JsonProperty("method") @BeanProperty method: String,
+         )
 
 
   def main(args: Array[String]): Unit = {
@@ -36,13 +46,53 @@ object Rainbow {
       .map {
         value => {
           val node = mapper.readValue(value.substring(value.indexOf('{')), classOf[MyValue])
-          ((node.method, node.path), 1)
+          ((node.method, node.path, node.user, node.ip), 1)
         }
       }.groupBy(0)
       .sum(1)
     counter.writeAsCsv(params.get("output"))
+
+    val counter2 = text.flatMap {
+      _.split("\n")
+    }
+      .map {
+        value => {
+          val node = mapper.readValue(value.substring(value.indexOf('{')), classOf[MyValue])
+          ((node.method, node.path, node.extra.errno), 1)
+        }
+      }.groupBy(0)
+      .sum(1)
+    counter2.writeAsCsv(params.get("output2"))
+
+//    val counter23 = text.flatMap {
+//      _.split("\n")
+//    }
+//      .map {
+//        value => {
+//          val node = mapper.readValue(value.substring(value.indexOf('{')), classOf[MyValue])
+//          ((node.method, node.path, translate(node.extra.executionTime)), 1)
+//        }
+//      }.groupBy(0)
+//      .sum(1)
+//    counter23.writeAsCsv(params.get("output23"))
+
+
+
     env.execute("helloworld")
 
+  }
+
+  def translate(time: Int): Int = {
+    if (0 < time && time < 100) {
+      return 0
+    } else if (100 < time && time < 200) {
+      return 1
+    } else if (200 < time && time < 500) {
+      return 2
+    } else if (500 < time && time < 1000) {
+      return 5
+    }
+    10
   }
 
 }
